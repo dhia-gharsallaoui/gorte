c5ab7fe48ccbc5e149698eecb25253b27b3450be
dhia gharsallaoui <safedhia@gmail.com>
2022-03-07 17:46:48 +0100

    add Signals API

--- client.go
+++ client.go
@@ -18,11 +18,14 @@ type ClientConfig struct {
 	authAdress string `default:"https://digital.iservices.rte-france.com/token/oauth/"`
 	host       string `default:digital.iservices.rte-france.com`
 	token      string
+	method     string // "GET" "POST"
+	apiAdress  string
 }
 
 type Client struct {
 	config ClientConfig
 	token  AuthToken
+	client *http.Client
 }
 
 func (c *Client) NewClient() error {
@@ -37,11 +40,10 @@ func (c *Client) NewClient() error {
 		c.config.host = "digital.iservices.rte-france.com"
 	}
 
+	c.client = &http.Client{}
 	ctype := "application/json"
 
 	auth := "Basic " + c.config.token
-
-	client := &http.Client{}
 
 	req, err := http.NewRequest("POST", c.config.authAdress, nil)
 	if err != nil {
@@ -50,7 +52,7 @@ func (c *Client) NewClient() error {
 
 	req.Header.Set("Content-Type", ctype)
 	req.Header.Set("Authorization", auth)
-	resp, err := client.Do(req)
+	resp, err := c.client.Do(req)
 	if err != nil {
 		return err
 	}
@@ -66,4 +68,15 @@ func (c *Client) NewClient() error {
 
 	c.token = token
 	return nil
+}
+
+func (c *Client) ConfigCheck() error {
+	if c.config.method == "" {
+		return errors.New("No method provided")
+	}
+	if c.config.apiAdress == "" {
+		return errors.New("No API adress provided in the client config")
+	}
+	return nil
+
 }
