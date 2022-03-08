729381ce1ffa29c2beab4e8e45f671c6eb67fe5e
dhia gharsallaoui <safedhia@gmail.com>
2022-03-08 15:33:27 +0100

    request abstaraction

--- signals.go
+++ signals.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"time"
 )
 
-type Signal struct {
-	Signals []struct {
+type GetSignalsOptions struct {
+	StartDate Time `url:"start_date"`
+	EndDate   Time `url:"end_date"`
+}
+
+type Signals struct {
+	Signal []struct {
 		StartDate time.Time `json:"start_date"`
 		EndDate   time.Time `json:"end_date"`
 		Type      string    `json:"type"`
@@ -22,74 +24,18 @@ type Signal struct {
 	} `json:"signals"`
 }
 
-type GridPeak struct {
-	StartDate  time.Time
-	EndDate    time.Time
-	Type       string // PP1/PP2 or  PP2 or NORMAL
-	UpdateDate time.Time
-}
+func (c *Client) GetSignals(opt *GetSignalsOptions) (*Signals, *http.Response, error) {
 
-func (c *Client) GetSignals(startDate, endDate interface{}) (*Signal, error) {
-	c.config.method = "GET"
-	c.config.apiAdress = "https://digital.iservices.rte-france.com/open_api/signal/v1/signals"
+	req, err := c.NewRequest("GET", "open_api/signal/v1/signals", opt)
 
-	err := c.ConfigCheck()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-
-	dd, err := dateParser(startDate, endDate)
+	var sig *Signals
+	resp, err := c.Do(req, &sig)
 	if err != nil {
-		return nil, err
+		return nil, resp, err
 	}
-	dates := *dd
-	req, err := http.NewRequest(c.config.method, c.config.apiAdress, nil)
-	if err != nil {
-		return nil, err
-	}
-	auth := c.token.TokenType + " " + c.token.AccessToken
-	req.Header.Set("Authorization", auth)
-	q := req.URL.Query()
-	q.Add("start_date", DateFormat(dates.startDate))
-	q.Add("end_date", DateFormat(dates.endDate))
-	req.URL.RawQuery = q.Encode()
-	resp, err := c.client.Do(req)
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	var respSignal Signal
-	if err := json.Unmarshal(body, &respSignal); err != nil {
-		return nil, err
-	} else {
-		log.Println("Signal catched !")
-	}
-	return &respSignal, nil
 
-}
-
-func (signal *Signal) ToGrid(onlyPP bool) []GridPeak {
-
-	var stype string
-	var peak GridPeak
-	gridpeak := make([]GridPeak, 0)
-	for i, ppsignal := range signal.Signals[0].Values {
-		if signal.Signals[0].Values[i].Value && signal.Signals[1].Values[i].Value {
-			stype = "PP1"
-		} else if signal.Signals[1].Values[i].Value {
-			stype = "PP2"
-		} else {
-			stype = "NORMAL"
-		}
-		if onlyPP == true {
-			if stype != "NORMAL" {
-				peak = GridPeak{ppsignal.StartDate, ppsignal.EndDate, stype, ppsignal.UpdatedDate}
-				gridpeak = append(gridpeak, peak)
-
-			}
-
-		} else {
-			peak = GridPeak{ppsignal.StartDate, ppsignal.EndDate, stype, ppsignal.UpdatedDate}
-			gridpeak = append(gridpeak, peak)
-		}
-	}
-	return gridpeak
+	return sig, resp, err
 }
