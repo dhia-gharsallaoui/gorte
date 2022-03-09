7a148fcb4dcd1f543c772425487a659d53089e58
dhia gharsallaoui <safedhia@gmail.com>
2022-03-09 15:51:04 +0100

    Add consumption Category and the Forecast API

---
+++ consumption.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+type AnnualForecastsResp struct {
+	AnnualForecasts []struct {
+		StartDate time.Time `json:"start_date"`
+		EndDate   time.Time `json:"end_date"`
+		Values    []struct {
+			StartDate                   time.Time `json:"start_date"`
+			EndDate                     time.Time `json:"end_date"`
+			AverageLoadSaturdayToFriday int       `json:"average_load_saturday_to_friday"`
+			AverageLoadMondayToSunday   int       `json:"average_load_monday_to_sunday"`
+			WeeklyMinimum               int       `json:"weekly_minimum"`
+			WeeklyMaximum               int       `json:"weekly_maximum"`
+			AverageLoadUpdatedDate      string    `json:"average_load_updated_date"`
+			MarginUpdatedDate           string    `json:"margin_updated_date"`
+			ForecastMargin              int       `json:"forecast_margin"`
+		} `json:"values"`
+	} `json:"annual_forecasts"`
+}
+
+func (co *Consumption) GetAnnualForecasts(opt *Period) (*AnnualForecastsResp, *http.Response, error) {
+	c := co.client
+	req, err := c.NewRequest(http.MethodGet, "open_api/consumption/v1/annual_forecasts", opt)
+	if err != nil {
+		return nil, nil, err
+	}
+	var sig *AnnualForecastsResp
+	resp, err := c.Do(req, &sig)
+	if err != nil {
+		return nil, resp, err
+	}
+	return sig, resp, err
+}
+
+type WeeklyForecastsResp struct {
+	WeeklyForecasts []struct {
+		EndDate time.Time `json:"end_date"`
+		Values  []struct {
+			EndDate   time.Time `json:"end_date"`
+			Value     int       `json:"value"`
+			StartDate time.Time `json:"start_date"`
+		} `json:"values"`
+		UpdatedDate time.Time `json:"updated_date"`
+		Peak        struct {
+			TemperatureDeviation float64   `json:"temperature_deviation"`
+			Value                int       `json:"value"`
+			PeakHour             time.Time `json:"peak_hour"`
+			Temperature          float64   `json:"temperature"`
+		} `json:"peak"`
+		StartDate time.Time `json:"start_date"`
+	} `json:"weekly_forecasts"`
+}
+
+func (co *Consumption) GetWeeklyForecasts(opt *Period) (*WeeklyForecastsResp, *http.Response, error) {
+	c := co.client
+	req, err := c.NewRequest(http.MethodGet, "open_api/consumption/v1/weekly_forecasts", opt)
+	if err != nil {
+		return nil, nil, err
+	}
+	var sig *WeeklyForecastsResp
+	resp, err := c.Do(req, &sig)
+	if err != nil {
+		return nil, resp, err
+	}
+	return sig, resp, err
+}
+
+type ShortTermResp struct {
+	ShortTerm []struct {
+		Type      string    `json:"type"`
+		StartDate time.Time `json:"start_date"`
+		EndDate   time.Time `json:"end_date"`
+		Values    []struct {
+			StartDate   time.Time `json:"start_date"`
+			EndDate     time.Time `json:"end_date"`
+			UpdatedDate time.Time `json:"updated_date"`
+			Value       int       `json:"value"`
+		} `json:"values"`
+	} `json:"short_term"`
+}
+
+func (co *Consumption) GetShortTerm(opt *Period) (*ShortTermResp, *http.Response, error) {
+	c := co.client
+	req, err := c.NewRequest(http.MethodGet, "open_api/consumption/v1/short_term", opt)
+	if err != nil {
+		return nil, nil, err
+	}
+	var sig *ShortTermResp
+	resp, err := c.Do(req, &sig)
+	if err != nil {
+		return nil, resp, err
+	}
+	return sig, resp, err
+}
