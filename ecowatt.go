aadd9da07a2eeadfd3a4410a4f87681251fa2d57
dhia gharsallaoui <safedhia@gmail.com>
2022-03-09 16:01:24 +0100

    Add ecowatt and quality API

---
+++ ecowatt.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+type SignalEcowatt struct {
+	Signals []struct {
+		StartDate time.Time `json:"start_date"`
+		EndDate   time.Time `json:"end_date"`
+		Values    struct {
+			Pdl []string `json:"PDL"`
+			Hdf []string `json:"HDF"`
+			Naq []string `json:"NAQ"`
+			Bfc []string `json:"BFC"`
+			Bre []string `json:"BRE"`
+			Pac []string `json:"PAC"`
+			Ara []string `json:"ARA"`
+			Occ []string `json:"OCC"`
+			Nor []string `json:"NOR"`
+			Idf []string `json:"IDF"`
+			Cvl []string `json:"CVL"`
+			Ges []string `json:"GES"`
+		} `json:"values"`
+	} `json:"signals"`
+}
+
+func (co *Consumption) GetSignalEcowatt(opt *Period) (*SignalEcowatt, *http.Response, error) {
+	c := co.client
+	req, err := c.NewRequest(http.MethodGet, "open_api/ecowatt/v3/signals", opt)
+	if err != nil {
+		return nil, nil, err
+	}
+	var sig *SignalEcowatt
+	resp, err := c.Do(req, &sig)
+	if err != nil {
+		return nil, resp, err
+	}
+	return sig, resp, err
+}
