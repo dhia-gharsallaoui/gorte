dd0329bdef01cc2078710ea4d679da95f56d0a9f
dhia gharsallaoui <dgharsallaoui@skilld.cloud>
2022-04-20 14:20:40 +0200

    add generation Forecast API

---
+++ generation_Forecast.go
@@ -0,0 +1,43 @@
+package gorte
+
+import (
+	"net/http"
+	"time"
+)
+
+type ForecastsResp struct {
+	Forecasts []struct {
+		StartDate      time.Time `json:"start_date"`
+		EndDate        time.Time `json:"end_date"`
+		Type           string    `json:"type"`
+		ProductionType string    `json:"production_type"`
+		Values         []struct {
+			StartDate   time.Time `json:"start_date"`
+			EndDate     time.Time `json:"end_date"`
+			UpdatedDate time.Time `json:"updated_date"`
+			Value       int       `json:"value"`
+		} `json:"values"`
+		SubType string `json:"sub_type,omitempty"`
+	} `json:"forecasts"`
+}
+
+type GetForecastsOptions struct {
+	StartDate Time   `url:"start_date"`
+	EndDate   Time   `url:"end_date"`
+	ProdType  string `url:"production_type"`
+	Type      string `url:"type"`
+}
+
+func (s *generation) GetForecasts(opt *GetForecastsOptions) (*ForecastsResp, *http.Response, error) {
+	c := s.client
+	req, err := c.NewRequest(http.MethodGet, "open_api/generation_forecast/v2/forecasts", opt)
+	if err != nil {
+		return nil, nil, err
+	}
+	sig := &ForecastsResp{}
+	resp, err := c.Do(req, sig)
+	if err != nil {
+		return nil, resp, err
+	}
+	return sig, resp, err
+}
