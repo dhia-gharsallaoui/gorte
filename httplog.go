d204c5caef777ad205bc5e7f2edae6046f742c71
dhia gharsallaoui <dgharsallaoui@skilld.cloud>
2022-04-28 13:43:29 +0200

    add http log

---
+++ httplog.go
@@ -0,0 +1,33 @@
+package gorte
+
+import (
+	"strings"
+)
+
+type httpLogger struct {
+	logger Logger
+}
+
+func NewHTTPLogger(l Logger) HTTPLogger {
+	return &httpLogger{logger: l}
+}
+
+func (l *httpLogger) Debug(msg string, keysAndValues ...interface{}) {
+	l.logger.Debug(msgToFormat(msg, keysAndValues...), keysAndValues...)
+}
+
+func (l *httpLogger) Info(msg string, keysAndValues ...interface{}) {
+	l.logger.Info(msgToFormat(msg, keysAndValues...), keysAndValues...)
+}
+
+func (l *httpLogger) Warn(msg string, keysAndValues ...interface{}) {
+	l.logger.Warn(msgToFormat(msg, keysAndValues...), keysAndValues...)
+}
+
+func (l *httpLogger) Error(msg string, keysAndValues ...interface{}) {
+	l.logger.Err(msgToFormat(msg, keysAndValues...), keysAndValues...)
+}
+
+func msgToFormat(msg string, keysAndValues ...interface{}) string {
+	return msg + strings.Repeat(" - %v: %v", len(keysAndValues)/2)
+}
